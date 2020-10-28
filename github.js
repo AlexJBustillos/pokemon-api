b4be1afa31912f4937020f36c90f82021c2cff59
alexjbustillos <alexjbustillos@gmail.com>
2020-10-28 11:36:34 -0400

    [github]add to index.html

--- github.js
+++ github.js
@@ -1,9 +1,22 @@
+const container = document.querySelector('.container');
+
+
 fetch('https://api.github.com/users/alexjbustillos')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const alexObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(alexObj);
+    // console.log(githubData);
+    const newElement = document.createElement('p');
+    newElement.textContent = alexObj.name;
+    container.appendChild(newElement);
+
 })
 
 .catch(error => {
